1089931bfbb4d621666a898c5783a008cbd16524
gamil91 <alexis_gamil99@yahoo.com.com>
2020-10-09 21:52:54 -0700

    Puppy lab

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+class Dog
+
+@@all = []
+
+def self.all
+    @@all
+end
+
+attr_reader :name
+
+def initialize(name)
+    @name = name
+    self.save
+end
+
+def self.print_all
+    @@all.each do |dog|
+    puts "#{dog.name}\n"
+    end
+end
+
+def save
+    @@all << self
+end
+
+def self.clear_all
+    @@all.clear
+end
+
+end
